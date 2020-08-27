2f88a53a94790d0afb87b35693a8279ca76990a7
lorry0508 <1165354824@qq.com>
2020-08-27 16:24:28 +0800

    dev

---
+++ Mobile/Vue-H5-Mobile-Template/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import { routerMap } from './router';
+
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location, onResolve, onReject) {
+    if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+    return originalPush.call(this, location).catch(err => err)
+}
+
+Vue.use(Router)
+
+const createRouter = () =>
+    new Router({
+        // mode: 'history', // 如果你是 history模式 需要配置vue.config.js publicPath
+        // base: process.env.BASE_URL,
+        scrollBehavior: () => ({ y: 0 }),
+        routes: routerMap
+    })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+    const newRouter = createRouter()
+    router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ Mobile/Vue-H5-Mobile-Template/src/router/router.js
@@ -0,0 +1,28 @@
+/**
+ *  基础路由
+ */
+export const routerMap = [{
+    path: '/',
+    component: () =>
+        import ('../views/layouts/index'),
+    redirect: '/home',
+    meta: {
+        title: '首页',
+        keepAlive: false
+    },
+    children: [{
+            path: '/home',
+            name: 'Home',
+            component: () =>
+                import ('../views/home/index'),
+            meta: { title: '首页', keepAlive: false }
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: () =>
+                import ('../views/home/about'),
+            meta: { title: '关于', keepAlive: false }
+        }
+    ]
+}];
